1611a024b4272719e33f8ed7ee51d49a484a5eeb
Christine Lee <chrstn.lee@mail.utoronto.ca>
2020-01-19 02:20:49 -0500

    add pages folder

---
+++ src/Pages/CreateProfile.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class CreateProfile extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Create your profile</h1>
+      </div>
+    );
+  }
+}
